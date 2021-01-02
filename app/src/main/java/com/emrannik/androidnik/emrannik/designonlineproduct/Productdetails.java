87fce155acc277e41b8e12f863d9a99c8650ffac
emran13 <emran.nannu@gmail.com>
2021-01-03 01:18:50 +0600

    Initial commit

---
+++ app/src/main/java/com/emrannik/androidnik/emrannik/designonlineproduct/Productdetails.java
@@ -0,0 +1,14 @@
+package com.emrannik.androidnik.emrannik.designonlineproduct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Productdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetails);
+    }
+}
